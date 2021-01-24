a622d7eb2bef858db120e28ceed51beda26f5c8d
MontiDon <yogi37893@gmail.com>
2021-01-24 17:49:55 +0300

    Lesson 63. axios.create

---
+++ api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        "API-KEY": "96774fe2-e841-46ad-a65b-44b215d2ff59"
+    }
+})
+
+export const userAPI = {
+    getUsers(currentPage = 1, pageSize = 11355) {
+        return (
+            instance.get(`users?page=${currentPage}&count=${pageSize}`, {}).then(response => {
+                return response.data;
+            })
+        )
+    }
+}
