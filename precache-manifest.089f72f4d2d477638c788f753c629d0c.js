a9a73d7da9ab52873299a114f4c322f9abce9dfa
MontiDon <yogi37893@gmail.com>
2021-03-10 15:11:27 +0300

    Updates

---
+++ precache-manifest.089f72f4d2d477638c788f753c629d0c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bac2a86daf93ee6d58b4d50a054efd",
+    "url": "/My-first-project/index.html"
+  },
+  {
+    "revision": "911a62582ec0201fa439",
+    "url": "/My-first-project/static/css/3.f0152bc7.chunk.css"
+  },
+  {
+    "revision": "baec14d3ab607c1f6b55",
+    "url": "/My-first-project/static/css/4.0d6eb449.chunk.css"
+  },
+  {
+    "revision": "92f8b0d0c9d0db3d9eac",
+    "url": "/My-first-project/static/css/main.e2f6ceb1.chunk.css"
+  },
+  {
+    "revision": "7474ded8fba89bbef1e6",
+    "url": "/My-first-project/static/js/2.7c6b08c7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/My-first-project/static/js/2.7c6b08c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911a62582ec0201fa439",
+    "url": "/My-first-project/static/js/3.fe81d97d.chunk.js"
+  },
+  {
+    "revision": "baec14d3ab607c1f6b55",
+    "url": "/My-first-project/static/js/4.7bdeca02.chunk.js"
+  },
+  {
+    "revision": "92f8b0d0c9d0db3d9eac",
+    "url": "/My-first-project/static/js/main.fb92107d.chunk.js"
+  },
+  {
+    "revision": "6476e3b0f14a06575ca9",
+    "url": "/My-first-project/static/js/runtime-main.f4317911.js"
+  },
+  {
+    "revision": "62ae644e21970d916566f01af2cdc7c1",
+    "url": "/My-first-project/static/media/preloader.62ae644e.svg"
+  }
+]);
